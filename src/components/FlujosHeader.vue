e354514758d264ab0d04c63da991575b71a0e23a
ericdc58 <heimricusdc@gmail.com>
2022-11-19 16:41:30 +0100

    First commit

---
+++ src/components/FlujosHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <img
+            @click="reload"
+            src="@/assets/wave_flow.svg" 
+            alt="Logo" 
+        />
+        <h1><span>Flujos</span>EDC</h1>
+        <span></span>
+    </header>
+</template>
+
+<script>
+export default {
+    methods: {
+        reload () {
+            location.reload();
+        }
+    }
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+}
+img,
+span {
+  width: 48px;
+}
+h1 {
+  font-size: 1.4rem;
+  color: var(--brand-silver);
+}
+h1 span {
+  color: var(--brand-gold);
+}
+</style>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="splashscreen">
+        <img 
+            src="@/assets/wave_flow.svg" 
+            alt="Logo"
+        />
+        <h1><span>Flujos</span>EDC</h1>
+    </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-silver);
+}
+h1 span {
+  color: var(--brand-gold);
+}
+</style>
